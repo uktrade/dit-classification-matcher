c3e06b08d5d8431250468c90171cd0142a070c51
alex giamas <agiamas@gmail.com>
2018-04-17 16:21:45 +0100

    (add) initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, make_response, url_for
+import urllib
+import json
+
+
+app = Flask(__name__, static_url_path = "")
+    
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+
+# load descriptions from json cpv vocabulary
+descriptions = json.loads(urllib.urlopen("files/main_vocabulary.json").read())
+
+def make_public_cpv(cpv):
+    new_cpv = {}
+    for field in cpv:
+        if field == 'id':
+            new_cpv['uri'] = url_for('get_cpv', cpv_id = cpv['id'], _external = True)
+        else:
+            new_cpv[field] = cpv[field]
+    return new_cpv
+
+@app.route('/api/v1/descriptions', methods = ['GET'])
+def get_descriptions():
+    return jsonify({'descriptions': map(make_public_cpv, descriptions)})
+
+@app.route('/api/v1/description/<int:cpv_id>', methods = ['GET'])
+def get_cpv(cpv_id):
+    cpv = filter(lambda t: t['id'] == cpv_id, descriptions)
+    if len(cpv) == 0:
+        abort(404)
+    return jsonify( { 'cpv': make_public_cpv(cpv[0]) } )
+    
+if __name__ == '__main__':
+    app.run(debug = True)
